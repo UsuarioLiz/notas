4af0abaac355d05e682384c6ba0edb94576389e5
Lizbeth Garcia Gil <lizbethgg07>
2022-04-07 12:03:11 -0500

    limpiar notas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState } from "react"
+
 function App() {
 
-  const [inputState, setInputState] = useState({
-    titulo: "",
-    fecha: "",
-    nota: "",
-  });
+ const [inputState, setInputState] = useState({
+   titulo: "", 
+   fecha: "", 
+   nota: "",
+ });
+
 
   const handleInputChange = (event) => {
     setInputState({
@@ -13,38 +15,67 @@ function App() {
       [event.target.name]: event.target.value,
     });
   };
-  
+
+  const handleClickLimpiar = ()=> {
+setInputState({titulo:"",fecha: "",nota:""});
+};
   return (
-    <div className="App">
+    <div className="App container">
+    <div className="row">
+    <div classMane="col">
+      <h3>lista</h3>
+    </div>
+    <div className="col">
       <h3>Notas</h3>
-      <label htmlFor="titulo">Título</label>
-      <input
-        id="titulo"
-        name="titulo"
-        type="text"
-        onChange={handleInputChange}
-        value={inputState.titulo}
+      <label className ="mb-2">
+        Titulo
+      <input 
+        id = "titulo" 
+        name = "titulo" 
+        type = "text" 
+        onChange = {handleInputChange}
+        value = {inputState.titulo}
       />
-     
-      <label htmlFor="fecha">Fecha</label>
-      <input
-         id="fecha"
-         name="fecha"
-         type="text"
-         onChange={handleInputChange}
-         value={inputState.fecha}
-      />
+      </label>
+      <br/>
 
-      <label htmlFor="nota">Nota</label>
-      <input
-         id="nota"
-         name="nota"
-         type="text"
-         onChange={handleInputChange}
-         value={inputState.nota}
+      <label className = "mb-2">
+        Fecha
+      <input 
+        id = "fecha"
+        name = "fecha" 
+        type = "text" 
+        onChange = {handleInputChange}
+        value = {inputState.fecha}
       />
+      </label>
+      <br/>
+
+      <label className = "mb-2">
+        Nota
+      <input 
+        id = "nota" 
+        name = "nota" 
+        type = "text" 
+        onChange = {handleInputChange}
+        value = {inputState.nota}
+      />
+      </label>
       </div>
-  );
-}
+      </div>
+      <hr />
+      <div className= "ms-2 mt-2">
+    <button
+          type="button" 
+          className="btn btn-primary" 
+          onClick={handleClickLimpiar}
+          >
+        Limpiar
+    </button>
 
+    </div>
+</div>
+  );
+  }
+  
 export default App;
